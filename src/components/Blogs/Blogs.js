b3811898dfd6a4874ea084b6ea2081940ded31bc
Rumana K <rumana2k20@gmail.com>
2022-04-05 04:29:24 +0600

    Blog and home text color chnaged

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -5,7 +5,7 @@ const Blogs = () => {
         <div className='mx-20'>
             <div className='my-10 border border-slate-300 bg-slate-200'>
                 <h2 className='text-xl px-5 text-orange-500 font-semibold' >What is Context API?</h2>
-                <p className='p-5 text-gray-500'>The React Context API is a way for a React app to effectively produce global variables which will be passed around. This is often the choice to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is additionally touted as a neater, lighter approach to state management using Redux.
+                <p className='p-5 text-slate-500'>The React Context API is a way for a React app to effectively produce global variables which will be passed around. This is often the choice to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is additionally touted as a neater, lighter approach to state management using Redux.
 
                     Context API is a (kind of) new feature added in version 16.3 of React that permits one to share state across the whole app (or a part of it) lightly and with ease.</p>
             </div>
@@ -13,7 +13,7 @@ const Blogs = () => {
             <div className='border border-slate-300 bg-slate-200'>
                 <h2 className='text-xl p-5 text-orange-500 font-semibold' >What is Semantic Tag?</h2>
 
-                <p className='px-5 pb-5 text-gray-500 '>A semantic tag clearly describes its meaning to both the browser and the developer.
+                <p className='px-5 pb-5 text-slate-500 '>A semantic tag clearly describes its meaning to both the browser and the developer.
                     And also, Semantic HTML5 addresses this shortcoming by defining specific tags to indicate clearly what role is played by the content those tags contain. That explicit information helps robots/crawlers like Google and Bing to better understand which content is important, which is a subsidiary, which is for navigation, and so on.
                 </p>
             </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,10 +15,10 @@ const Home = () => {
 
     return (
         <div>
-            <div className=' my-10 mx-20 flex leading-normal'>
+            <div className=' my-10 mx-20 flex leading-normal text-slate-500'>
                 <div className='w-full'> <img src="https://images.unsplash.com/photo-1625850810879-63fa66c9d0b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" alt="" srcset="" /> </div>
                 <div className='m-10'>
-                    <h1 className=' leading-loose text-3xl font-extrabold'>Most Popular and Best Selling Book. <br /> <span className='text-sky-800 text-4xl' >That can make your life meaningful.</span></h1>
+                    <h1 className=' leading-loose text-3xl font-extrabold'>Book Analysis Website. <br /> <span className='text-sky-800 text-4xl' >Ikigai can make your life meaningful.</span></h1>
                     <p className=' p-5 '>Ikigai gently unlocks simple secrets we can all use to live long, meaningful, happy lives. Science-based studies weave beautifully into honest, straight-talking conversation you won't be able to put down. Warm, patient, and kind, this book pulls you gently along your own journey rather than pushing you from behind.</p>
                     <button className='py-2 px-8 bg-sky-500 text-white font-semibold rounded-lg shadow-md hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75'>SEE MORE</button>
                 </div>
