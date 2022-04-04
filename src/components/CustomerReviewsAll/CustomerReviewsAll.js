55b8efd9335f80ab5da5c7b83fb20686f93464aa
Rumana K <rumana2k20@gmail.com>
2022-04-04 06:14:18 +0600

    Data loaded on home and review components

--- src/components/CustomerReviewsAll/CustomerReviewsAll.js
+++ src/components/CustomerReviewsAll/CustomerReviewsAll.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const CustomerReviews = (props) => {
-    
-    const {name, comment, icon}=props.review
-    
+const CustomerReviewsAll = (props) => {
+    const { name, comment, icon } = props.review
     return (
         <div>
             <h2>{name}</h2>
             <p>{comment}</p>
             <p>{icon}</p>
-            
+
         </div>
-        
+
     );
 };
 
-export default CustomerReviews;
+export default CustomerReviewsAll;
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import useReviews from '../../hooks/useReviews';
 import CustomerReviews from '../CustomerReviews/CustomerReviews';
+import CustomerReviewsAll from '../CustomerReviewsAll/CustomerReviewsAll';
 
 const Reviews = () => {
     const [reviews, setReviews] = useReviews();
     
     return (
         <div>
-            <h2>review {reviews.length}</h2>
-            {
-                reviews.map(review => <CustomerReviews key ={review.id} review={review} ></CustomerReviews> )
-            }
+            <h2>Total Review: {reviews.length}</h2>
+            {reviews.map(review => <CustomerReviewsAll key ={review.id} review={review}></CustomerReviewsAll>)}
         </div>
     );
 };
